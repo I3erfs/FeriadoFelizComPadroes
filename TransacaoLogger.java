290c7d44f2e9ae36a4c5639eba94781cf6b739d5
Bernardo Friske <148400713+I3erfs@users.noreply.github.com>
2026-07-01 21:54:15 -0300

    Refactor TransacaoLogger to Logger and add factory

--- TransacaoLogger.java
+++ TransacaoLogger.java
@@ -1,9 +1,21 @@
-class TransacaoLogger {
-    private static TransacaoLogger instancia;
-    private TransacaoLogger() {} // Construtor privado
-    public static TransacaoLogger getInstancia() {
-        if (instancia == null) instancia = new TransacaoLogger();
+//  SINGLETON: Log de Transações
+class Logger {
+    private static Logger instancia;
+    private Logger() {}
+    public static Logger getInstancia() {
+        if (instancia == null) instancia = new Logger();
         return instancia;
     }
-    public void logar(String msg) { System.out.println("[AUDITORIA] " + msg); }
+    public void log(String msg) { System.out.println("[LOG]: " + msg); }
+}
+
+// FACTORY: Criação de produtos
+interface Produto { String getNome(); }
+class ProdutoFisico implements Produto { public String getNome() { return "Produto Físico"; } }
+
+class ProdutoFactory {
+    public static Produto criar(String tipo) {
+        if (tipo.equals("fisico")) return new ProdutoFisico();
+        return null;
+    }
 }
